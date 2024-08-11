5663499690d5eb887f15b9b668fa4cead43ce717
Samuditha-Rashmika <rashmikadesilva03@gmail.com>
2024-08-11 06:23:16 +0530

    Project Setup

---
+++ src/Asp.NetCore-WebApi-Clean-Architecture/WebApp/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Application;
+using Infrastrutcure;
+
+namespace WebApi
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddWebApiDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI()
+                .AddInfrastrutcureDI();
+
+            return services;
+        }
+       
+    }
+}
